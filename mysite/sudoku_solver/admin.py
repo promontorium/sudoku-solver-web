1ca91d3d220d605b99c4d59deaf43eb7cf3aeed6
rqc <rqc@rqc.com>
2025-03-24 03:05:35 +0300

    admin board intial

--- mysite/sudoku_solver/admin.py
+++ mysite/sudoku_solver/admin.py
@@ -4,3 +4,8 @@ from django.contrib.auth.admin import UserAdmin
 from . import models
 
 admin.site.register(models.User, UserAdmin)
+
+
+@admin.register(models.Board)
+class BoardAdmin(admin.ModelAdmin):
+    pass
