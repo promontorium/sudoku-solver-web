2ffe67c053aacd276b7a93504b3fee4475be8252
rqc <rqc@rqc.com>
2025-03-27 11:28:54 +0300

    .

--- mysite/sudoku_solver/static/ts/password-widget.ts
+++ mysite/sudoku_solver/static/ts/password-widget.ts
@@ -1,14 +1,18 @@
 window.addEventListener("load", () => {
     const handlerSetAttribute = "password-widget-reveal-handler-set";
+    const SHOW_TITLE = "Show password";
+    const HIDE_TITLE = "Hide password";
     document.querySelectorAll(".password-widget-reveal-wrapper").forEach((element) => {
         if (!element.hasAttribute(handlerSetAttribute)) {
+            element.setAttribute("title", SHOW_TITLE);
             element.addEventListener("click", () => {
-                element.querySelectorAll(".password-widget-reveal").forEach((svg) => {
-                    svg.classList.toggle("hidden");
-                });
                 const input = element.parentElement?.querySelector("input") as HTMLInputElement;
                 if (input) {
                     input.type = input.type === "password" ? "text" : "password";
+                    element.setAttribute("title", input.type === "password" ? SHOW_TITLE : HIDE_TITLE);
+                    element.querySelectorAll(".password-widget-reveal").forEach((svg) => {
+                        svg.classList.toggle("hidden");
+                    });
                 }
             });
             element.setAttribute(handlerSetAttribute, "true");
