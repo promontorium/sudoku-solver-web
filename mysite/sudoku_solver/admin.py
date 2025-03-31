8bebb210b3a9f615cbd624667a140b51d36f2030
rqc <rqc@rqc.com>
2025-04-01 00:38:26 +0300

    autocomplete user fk in admin panel

--- mysite/sudoku_solver/admin.py
+++ mysite/sudoku_solver/admin.py
@@ -10,6 +10,7 @@ admin.site.register(models.User, UserAdmin)
 class BoardAdmin(admin.ModelAdmin):
     form = forms.AdminBoardForm
     list_display = ("id", "user", "description", "created", "changed")
+    autocomplete_fields = ("user",)
     list_filter = (("created", admin.DateFieldListFilter), ("changed", admin.DateFieldListFilter))
     search_fields = ("id", "description", "created", "changed")
     list_display_links = ("description",)
